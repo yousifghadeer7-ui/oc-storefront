ad8ae481521abd46c60406432e976769507ea4bd
yousifghadeer7-ui <yousifghadeer7@gmail.com>
2026-09-08 22:49:06 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.shopify.com",
-      },
+      { protocol: "https", hostname: "**" },
     ],
   },
 };
