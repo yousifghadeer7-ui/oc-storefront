b93abaa60545dd3528b21644bdd765ca9fb3ecf5
yousifghadeer7-ui <yousifghadeer7@gmail.com>
2026-09-09 23:36:46 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // يتجاهل أخطاء TypeScript ويجبر Vercel على إكمال النشر فوراً
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // يتجاهل أخطاء ESLint أثناء الـ Build
-    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
